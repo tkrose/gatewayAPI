949e0113b2ea521951a2d8b00d9654899bf7ffd0
Talia Kaplanian <tkaplani@redhat.com>
2020-07-08 10:00:09 -0400

    Added back app route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import connexion
 import requests
-import client, gateway, baremetal, vm
+import gateway, baremetal, virtualmachine
 
 #Create the application instance
 app = connexion.App(__name__, specification_dir='./')
@@ -9,10 +9,10 @@ app = connexion.App(__name__, specification_dir='./')
 #Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
 
-#Create a URL route in our application for "/" ; REMOVE ALL OF THIS???
-#@app.route('/', methods=["GET","POST"])
-#def home():
-    #return render_template('home.html')
+#Create a URL route in our application for "/"
+@app.route('/', methods=["GET","POST"])
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
