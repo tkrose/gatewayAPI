b3fe29005c078d031afc4745f7bf90f877c60aa8
Talia Kaplanian <tkaplani@redhat.com>
2020-07-08 16:03:47 -0400

    Removed extra print statement

--- client.py
+++ client.py
@@ -6,7 +6,6 @@ from constants import headers
 # Read in the json
 with open('requirements.json') as f:
    fileData = open('requirements.json', 'rb')
-print(type(fileData))
 
 # Post to gateway API
 requests.post('http://localhost:8080/api/requirements', headers=headers, data=fileData)
