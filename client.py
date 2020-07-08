8ad91014ff154adf65c3ab73c2860a49162e269b
Talia Kaplanian <tkaplani@redhat.com>
2020-07-08 16:10:31 -0400

    Added OpenShift URL

--- client.py
+++ client.py
@@ -8,4 +8,5 @@ with open('requirements.json') as f:
    fileData = open('requirements.json', 'rb')
 
 # Post to gateway API
-requests.post('http://localhost:8080/api/requirements', headers=headers, data=fileData)
+#requests.post('http://localhost:8080/api/requirements', headers=headers, data=fileData) # Localhost URL
+requests.post('http://gateway-api-git-rp-v5-dev.apps.ocp.prod.psi.redhat.com/api/requirements', headers=headers, data=fileData) # OpenShift URL
