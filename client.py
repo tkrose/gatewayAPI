509e91330732a4abf0f2fc8d0a084b21a35a8f70
Talia Kaplanian <tkaplani@redhat.com>
2020-07-08 15:42:50 -0400

    Removed hardcoded data

--- client.py
+++ client.py
@@ -3,34 +3,6 @@ import requests
 from flask import make_response, abort
 from constants import headers
 
-# Data to serve with our API
-requirements = {
-    "baremetal": {
-        "name": "baremetal-node",
-        "role": "node",
-        "provider": {
-           "name": "baremetal",
-           "credential": "baremetal-creds",
-           "image": "rhel-7.5-server-x86_64-released",
-           "networks": [
-              "{{ network }}"
-           ]
-        }
-     },
-    "virtual-machine": {
-        "name": "virtual-machine-node",
-        "role": "node",
-        "provider": {
-           "name": "virtual-machine",
-           "credential": "virtual-machine-creds",
-           "image": "rhel-7.5-server-x86_64-released",
-           "networks": [
-              "{{ network }}"
-           ]
-        }
-    }
-  }
-
 # Read in the json
 with open('requirements.json') as f:
    fileData = open('requirements.json', 'rb')
