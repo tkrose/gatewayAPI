ad2145fcf80b3488b2046be23abf4817d73b5ea5
Talia Kaplanian <tkaplani@redhat.com>
2020-07-08 10:01:19 -0400

    Renamed vm.py to virtualmachine.py

--- virtualmachine.py
+++ virtualmachine.py
@@ -1,9 +1,8 @@
-#accept post
 from flask import Flask, render_template, make_response, abort
 import connexion
 import requests
-import json
 
 
 def vm_post(vmReq):
-    return reqObj = json.loads(vmReq.text)
+    print(vmReq)
+    return vmReq
